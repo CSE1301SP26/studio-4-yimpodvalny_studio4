173a643cba0a988cc5d9f21ade642ccd63e752a5
elizapodvalny <e.podvalny@wustl.edu>
2026-02-18 14:48:44 -0600

    half right

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,18 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int colorR = in.nextInt();
+		int colorG = in.nextInt();
+		int colorB = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextInt();
+		double y = in.nextInt();
+		double semiMajorAxis = in.nextInt();
+		double semiMinorAxis = in.nextInt();
+		StdDraw.setPenColor(colorR, colorG, colorB);
+		StdDraw.filledEllipse (x, y, semiMajorAxis, semiMinorAxis);
+
+
 	}
 }
